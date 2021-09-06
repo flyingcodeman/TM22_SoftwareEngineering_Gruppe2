d9d59207295569e6dfddf4b33c33abc3b1ec269c
flyingcodeman <66323964+flyingcodeman@users.noreply.github.com>
2021-09-06 18:32:46 +0200

    Initial commit

---
+++ firstFile.java
@@ -0,0 +1,10 @@
+public class firstClass {
+
+    /*
+     * Initial commit
+     */
+
+    public static void main(String []args) {
+        System.out.println("Hello World"); // prints Hello World
+    }
+}
