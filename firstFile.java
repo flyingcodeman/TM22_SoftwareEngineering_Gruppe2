0c2446f1d19f3d38985e33253a9906910eca1120
flyingcodeman <66323964+flyingcodeman@users.noreply.github.com>
2021-09-06 23:44:08 +0200

    First thoughts on possible methods

--- firstFile.java
+++ firstFile.java
@@ -1,8 +1,13 @@
-public class firstClass {
+public class battleship {
 
     /*
      * Initial commit
      */
+    public void createPitch(){}
+    public void placeShip(){}
+    public void sankShip(){}
+    public void shoot(){}
+    public void showOnConsole(){}
 
     public static void main(String []args) {
         System.out.println("Hello World"); // prints Hello World
