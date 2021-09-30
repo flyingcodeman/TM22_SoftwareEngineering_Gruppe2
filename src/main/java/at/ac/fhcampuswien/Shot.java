e34fea72045fee866f6e122f7d183a8509a6a3d7
flyingcodeman <66323964+flyingcodeman@users.noreply.github.com>
2021-09-30 19:55:33 +0200

    Implemented first basic functionality of char detection with coordinates

--- src/main/java/at/ac/fhcampuswien/Shot.java
+++ src/main/java/at/ac/fhcampuswien/Shot.java
@@ -6,6 +6,20 @@ public class Shot {
     private Player targetPlayer;
 
     public void shootsAt (Coordinate coordinate, Player player) {
-        //
+        char[][] currentTargetField = player.getOwnField();
+        if(currentTargetField[coordinate.positionY][coordinate.positionX] == '~'){
+            System.out.println("WATER");
+        }else if(currentTargetField[coordinate.positionY][coordinate.positionX] == 'X' ||
+                currentTargetField[coordinate.positionY][coordinate.positionX] == '#'){
+            System.out.println("Already shot!");
+        }else if(currentTargetField[coordinate.positionY][coordinate.positionX] == '1' ||
+                currentTargetField[coordinate.positionY][coordinate.positionX] == '2' ||
+                currentTargetField[coordinate.positionY][coordinate.positionX] == '3' ||
+                currentTargetField[coordinate.positionY][coordinate.positionX] == '4' ||
+                currentTargetField[coordinate.positionY][coordinate.positionX] == '5'){
+            System.out.println("HIT!");
+            player.fleet.fleet[3].ship[1].position[0].shotAt = true;
+            player.hit(coordinate);
+        }
     }
 }
