198c8cd1334668fc708723efe523d7e629df5715
flyingcodeman <66323964+flyingcodeman@users.noreply.github.com>
2021-10-01 00:27:22 +0200

    Extended shootAt method

--- src/main/java/at/ac/fhcampuswien/Shot.java
+++ src/main/java/at/ac/fhcampuswien/Shot.java
@@ -4,32 +4,47 @@ public class Shot {
 
     private Coordinate target;
     private Player targetPlayer;
+    public enum State {
+        reload, hit, miss, error
+    }
 
     //returns true if the shot was valid, returns false if invalid
-    public boolean shootsAt (Coordinate coordinate, Player player) {
-        char[][] field = (player.getOpponent()).getOwnField();
-        char character = player.getCharFromOpponentFieldAtCoordinate(coordinate, field);
+    public State shootsAt (Coordinate coordinate, Player currentPlayer, Player currentOpponent) {
+        State resultState;
+        char[][] targetField = currentOpponent.getOwnField();
+        char character = currentOpponent.getCharAtPosition(coordinate);
         if (character == '-' ||
             character == 'X' ||
             character == '#') {
-            // schonmal geschossen == invalide
-            System.out.println("You already shot there. Please try again");
-            return false;
-            }
-        else if ( character == '2' ||
+            // already shot == invalid
+            resultState = State.reload;
+        }
+        else if(character == '~'){
+            currentPlayer.setCharAtPositionOpponentInfoField('-', coordinate);
+            resultState = State.miss;
+        }
+        else if (character == '2' ||
                 character == '3' ||
                 character == '4'||
                 character == '5') {
             // shot hits a ship
             // muss dem schiff, dass bei dem Opponent an der Coordinate coordinate steht sagen, dass es getroffen wurde ^^
             // setz das Zeichen beim player.opponentfield von ~ auf X o #
-            return true;
+            currentOpponent.setCharAtPositionOwnField('X', coordinate);
+            currentPlayer.setCharAtPositionOpponentInfoField('X', coordinate);
+            if(currentOpponent.fleet.checkIfShipSunk(coordinate)){
+                System.out.println("Treffer und Schiff VERSENKT");
+            }
+            if(currentOpponent.fleet.checkIfFleetSunk()){
+                currentOpponent.gameOver = true;
+            }
+            resultState = State.hit;
         }
-
         else {
-            // invalider schuss da nicht wasser aber auch kein schiff von 2 - 5
-            return false;
+            // invalider schuss
+            // ToDo: Catch Error!
+            resultState = State.error;
         }
-
+        return resultState;
     }
 }
