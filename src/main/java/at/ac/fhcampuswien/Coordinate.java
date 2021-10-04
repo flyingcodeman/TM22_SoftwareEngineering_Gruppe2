515673166853a6812f5cffe648ab8fe0ee48ee63
flyingcodeman <66323964+flyingcodeman@users.noreply.github.com>
2021-10-04 20:26:22 +0200

    Added coordinate translation for x-Coordinate (Char to int)

--- src/main/java/at/ac/fhcampuswien/Coordinate.java
+++ src/main/java/at/ac/fhcampuswien/Coordinate.java
@@ -19,4 +19,42 @@ public class Coordinate {
     public int getPositionX(){return positionX;}
     public int getPositionY(){return positionY;}
 
+    public int translateInput(char c){
+        int xCoordinate = 0;
+        if(c == 'A'){
+            xCoordinate = 0;
+        }
+        else if(c == 'B'){
+            xCoordinate = 1;
+        }
+        else if(c == 'C'){
+            xCoordinate = 2;
+        }
+        else if(c == 'D'){
+            xCoordinate = 3;
+        }
+        else if(c == 'E'){
+            xCoordinate = 4;
+        }
+        else if(c == 'F'){
+            xCoordinate = 5;
+        }
+        else if(c == 'G'){
+            xCoordinate = 6;
+        }
+        else if(c == 'H'){
+            xCoordinate = 7;
+        }
+        else if(c == 'I'){
+            xCoordinate = 8;
+        }
+        else if(c == 'J'){
+            xCoordinate = 9;
+        }
+        else{
+            System.out.println("Translation error: Invalid character!");
+        }
+        return xCoordinate;
+    }
+
 }
