5becb1c2f46875501cd45f27ceee73b309e9ab45
flyingcodeman <66323964+flyingcodeman@users.noreply.github.com>
2021-10-17 18:09:26 +0200

    TBC Implementation of 40.02 & 40.08

--- src/test/java/at/ac/fhcampuswien/core/PlayerTest.java
+++ src/test/java/at/ac/fhcampuswien/core/PlayerTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class PlayerTest {
 
     @Test
-    void printFieldSetTest() { //TC-40.01
+    void printFieldSetTest01() { //TC-40.01
         ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
 
@@ -29,5 +29,43 @@ class PlayerTest {
         assertTrue(outContent.toString().contains(expectedOutShip2));
         assertTrue(outContent.toString().contains(expectedOutShip3));
         assertTrue(outContent.toString().contains(expectedOutWater));
+    }
+
+    void printFieldSetTest0509() { //TC-40.05 // TC-40.09
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        Player player1 = new Player("Test1");
+        //Player player2 = new Player("Test2");
+        player1.setStandardFleet();
+        player1.printFieldset();
+
+        String expectedOutNotHitYet = "~";
+
+        assertTrue(outContent.toString().contains(expectedOutNotHitYet));
+
+    }
+
+    void printFieldSetTest0208() { //TC-40.02 // TC-40.08
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+
+        Player player1 = new Player("Test1");
+        Player player2 = new Player("Test2");
+        player1.setStandardFleet();
+        player2.setStandardFleet();
+        Shot shoot = new Shot();
+        Coordinate givenShootCoordinate = new Coordinate(0,0);
+        givenShootCoordinate.setNewCoordinates('A', 1);
+        //result = shoot.shootsAt(givenShootCoordinate, player2, player1);
+
+
+        player1.printFieldset();
+
+        String expectedOutNotHitYet = "~";
+
+        assertTrue(outContent.toString().contains(expectedOutNotHitYet));
+
     }
 }
