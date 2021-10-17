54d54de5d5db76e5cb23085590c4f2217c67a043
Chirs <christopher.moser@stud.fh-campuswien.ac.at>
2021-10-17 20:58:14 +0200

    Changed position for welcome rules

--- src/test/java/at/ac/fhcampuswien/core/ShootStatusTest.java
+++ src/test/java/at/ac/fhcampuswien/core/ShootStatusTest.java
@@ -8,9 +8,14 @@ class ShootStatusTest {
 
     @Test
     void checkShootOnUsedField() {
-        Shot shootAt = new Shot();
+        Shot shoot = new Shot();
+        Player player1 = new Player('player0ne');
+        Player player2 = new Player('playerTwo');
         Coordinate givenShootCoordinate = new Coordinate(0,0);
-        Shot.State result = Shot.State.reload;
+        Shot.State result;
+        player2[1][1] = '~';
+
+        result = shoot.shootsAt(givenShootCoordinate, player1, player2);
         assertEquals(Shot.State.reload, result);
     }
     @Test
