6b8ad403e9716cb6271d135338cdc0ffa39e3869
Chirs <christopher.moser@stud.fh-campuswien.ac.at>
2021-10-17 18:45:27 +0200

    Added Test Cases for ShootStatusTest
    Error in requesting state (hit, reload,...)
    
    tbd

---
+++ src/test/java/at/ac/fhcampuswien/core/ShootStatusTest.java
@@ -0,0 +1,23 @@
+package at.ac.fhcampuswien.core;
+
+import org.junit.jupiter.api.Test;
+
+@Test
+class ShootStatusTest {
+
+    @Test
+    void checkShootOnUsedField() {
+        Shot shootAt = new Shot();
+        Coordinate givenShootCoordinate = new Coordinate(0,0);
+        Shot.State result = Shot.State.reload;
+
+    }
+    @Test
+    void checkShootOnWater() {
+        Player player1 = new Player('player0ne');
+        Player player2 = new Player('playerTwo');
+        Shot.State result = Shot.State.reload;
+
+    }
+}
+
