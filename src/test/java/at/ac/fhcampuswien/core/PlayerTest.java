664f54fe584442b1cfe4395093288a087b2edaea
flyingcodeman <66323964+flyingcodeman@users.noreply.github.com>
2021-10-17 18:00:54 +0200

    Added TC-40.01 / printFieldSetTest

--- src/test/java/at/ac/fhcampuswien/core/PlayerTest.java
+++ src/test/java/at/ac/fhcampuswien/core/PlayerTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class PlayerTest {
 
     @Test
-    void printFieldSetTest() {
+    void printFieldSetTest() { //TC-40.01
         ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
 
@@ -19,31 +19,15 @@ class PlayerTest {
         player1.setStandardFleet();
         player1.printFieldset();
 
-        String expectedOut1 = "A B C D E F G H I J";
-        String expectedOut2 = "2 2";
-        String expectedOut3 = "3 3 3";
-
-        assertTrue(outContent.toString().contains(expectedOut1));
-        assertTrue(outContent.toString().contains(expectedOut2));
-        assertTrue(outContent.toString().contains(expectedOut3));
-    }
+        String expectedOutAxis = "A B C D E F G H I J";
+        String expectedOutShip2 = "2 2";
+        String expectedOutShip3 = "3 3 3";
+        String expectedOutWater = "~";
 
 
-
-
-    boolean checkArrayFields(char[][] checkField, int size){
-        int counter = 0;
-        for(int x = 0; x<checkField.length; x++){
-            for(int y = 0; y<checkField.length; y++){
-                if(checkField[x][y] == '~'){
-                    counter++;
-                }
-            }
-        }
-        if(counter == size * size){
-            return true;
-        }else{
-            return false;
-        }
+        assertTrue(outContent.toString().contains(expectedOutAxis));
+        assertTrue(outContent.toString().contains(expectedOutShip2));
+        assertTrue(outContent.toString().contains(expectedOutShip3));
+        assertTrue(outContent.toString().contains(expectedOutWater));
     }
 }
