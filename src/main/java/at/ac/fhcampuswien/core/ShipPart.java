a5c2b8365616bb781231559126a71802ea5b4d3a
flyingcodeman <66323964+flyingcodeman@users.noreply.github.com>
2021-11-05 10:58:32 +0100

    Refactored: Removed not needed spaces and methods adjusted to new objects and parameters from other classes

--- src/main/java/at/ac/fhcampuswien/core/ShipPart.java
+++ src/main/java/at/ac/fhcampuswien/core/ShipPart.java
@@ -1,35 +1,17 @@
 package at.ac.fhcampuswien.core;
 
-import at.ac.fhcampuswien.core.Coordinate;
-
 public class ShipPart {
 
-    public Coordinate[] position; //1-Dimensionaler Koordinaten-Array zur Speicherung von x & y!
-    private boolean hit; // Variable zur Speicherung, ob Schiffsteil getroffen wurde //ToDo: Speichern im position-Array!
+    public Coordinate position;
+    private boolean hit;
 
-    //Constructor zur Initialisierung des Schiffsteils
     public ShipPart(int x, int y){
-        position = new Coordinate[1];
-        position[0] = new Coordinate(x,y);
+        position = new Coordinate(x,y);
         hit = false;
     }
 
-    // Wenn Treffer erfolgreich: Setzen der Varibale hit = true
     public void setHit(){hit = true;}
-
-    // Rückgabe des aktuellen Status von hit
     public boolean getHit(){
         return hit;
     }
-
-    /*public int[] getCoordinates(){
-        int[] coordinates = new int[2];
-        coordinates[0] = position.getPositionX();
-        coordinates[1] = position.getPositionY();
-        return coordinates;
-    }*/
-
-    /*public void setCoordinates(int x, int y){
-        position.setCoordinates(x,y);
-    }*/
 }
