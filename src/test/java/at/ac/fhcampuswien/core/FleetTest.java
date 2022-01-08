44d9e266bde39a784e2d93513c7b4aa9175cee57
flyingcodeman <accountmails@icloud.com>
2022-01-08 22:21:59 +0100

    Updated, checked and committed realized TCs

--- src/test/java/at/ac/fhcampuswien/core/FleetTest.java
+++ src/test/java/at/ac/fhcampuswien/core/FleetTest.java
@@ -6,14 +6,14 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class FleetTest {
+class FleetTest { // TC-44 // TC-44.010
     @Test
     void checkIfFleetSunkTest() {
         Field field = new Field(10);
         Fleet fleet = new Fleet();
         fleet.createStandardFleet(field);
 
-        //Setzen alles Schiffe auf "sank"
+        //Setzen aller Schiffe auf "sank"
         for (int i = 0; i < 10; i++) { // Iteration über alle Schiffe der Flotte
             for (int j = 0; j < fleet.fleet[i].ship.length; j++) { // Iteration über alle shipParts des aktuell ausgewählten Schiffs
                 fleet.fleet[i].ship[j].setHit();
